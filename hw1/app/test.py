0640480e6605ee2a9f56e1539bf7a78d71eaa164
Maksim <savkin.mk@phystech.edu>
2022-12-09 19:29:34 +0300

    add code coverage

--- hw1/app/test.py
+++ hw1/app/test.py
@@ -1,2 +1,7 @@
+from app import home
+
+def test_main_page():
+    assert home() == "Cacti with spike length >= 3: [('rebutia', 'senilis', 3), ('rebutia', 'albiflora', 3), ('echinopsis', 'amblayensis', 3), ('echinopsis', 'albispinosa', 4), ('mammillaria', 'bocasana', 4)]"
+
 def test_nothing():
     assert True
